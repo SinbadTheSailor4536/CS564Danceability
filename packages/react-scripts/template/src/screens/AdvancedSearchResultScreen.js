6d1a401ef8ca010374c9ee52c9a4093178ca0400
Liam Tomson-Moylan <tomsonmoylan@wisc.edu>
2019-08-07 14:07:29 -0500

    Added screens with navigation and mocked up a home screen

---
+++ packages/react-scripts/template/src/screens/AdvancedSearchResultScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import {
+    View,
+    Platform,
+    Alert,
+    ScrollView,
+    RefreshControl,
+    Dimensions
+  } from "react-native";
+  import { Button, Text } from "react-native-elements";
