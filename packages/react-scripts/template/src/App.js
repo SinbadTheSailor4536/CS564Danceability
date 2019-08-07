99adbbaf95a3c47438cb6fbc7b20f3149901592d
Liam Tomson-Moylan <tomsonmoylan@wisc.edu>
2019-08-06 20:18:54 -0500

    Removed React logo and Updated color scheme and Display for Home Page

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -6,13 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>
           Danceability
         </h1>
-        <a>
+        <p>
           An app for Dancing Musicality
-        </a>
+        </p>
       </header>
     </div>
   );
