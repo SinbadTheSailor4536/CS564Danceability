9c4367b1fb857dd83ae4b28c5a4a5c7317c416f3
Liam Tomson-Moylan <tomsonmoylan@wisc.edu>
2019-08-08 10:16:23 -0500

    updated import of SearchBar to SongSearchBar

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/SongSearchBar';
+import SongSearchBar from './components/SongSearchBar';
 
 function App() {
   return (
