89ea4f659bd20e304eaa4a23bd7373e2c54d57f6
Liam Tomson-Moylan <tomsonmoylan@wisc.edu>
2019-08-08 11:36:36 -0500

    Removed blank space, restoring backgroud

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -5,14 +5,16 @@ import SongSearchBar from './components/SongSearchBar';
 
 function App() {
   return (
-    <div className="center">
-      <header className="App-header">
-        <h1>Danceability</h1>
-        <p>An app for Dancing Musicality</p>
-      </header>
-      <div className="left">
-        <h2>Quick Search</h2>
-        <SongSearchBar />
+    <div className="backgroud">
+      <div className="center">
+        <header className="App-header">
+          <h1>Danceability</h1>
+          <p>An app for Dancing Musicality</p>
+        </header>
+        <div className="left">
+          <h2>Quick Search</h2>
+          <SongSearchBar />
+        </div>
       </div>
     </div>
   );
