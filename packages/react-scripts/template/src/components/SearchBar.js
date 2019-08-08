e8652143c50bd8d00389d90975cfcadf7aca62b3
Liam Tomson-Moylan <tomsonmoylan@wisc.edu>
2019-08-07 20:58:12 -0500

    added Search Bar

---
+++ packages/react-scripts/template/src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A search was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Dance Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<SearchBar />, document.getElementById('root'));
