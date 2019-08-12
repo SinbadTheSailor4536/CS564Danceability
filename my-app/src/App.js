7e2318ea22ed16e9f78d6087e8ab6a8abd49c6f4
unknown <alqaryan@wisc.edu>
2019-08-12 11:39:22 -0500

    Home page layout ready

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SongSearchBar from './components/SongSearchBar';
+import DurationFields from './components/DurationFields';
+import ReleaseYearRange from './components/ReleaseYearRange';
+import DanceSearchMenu from './components/DanceSearchMenu';
+import OrderByPopularity from './components/OrderByPopularity';
+import AdvancedSearch from './components/AdvancedSearch';
 
 function App() {
   return (
@@ -14,6 +19,10 @@ function App() {
         <div className="left">
           <h2>Quick Search</h2>
           <SongSearchBar />
+          <DanceSearchMenu />
+
+          <AdvancedSearch />
+          
         </div>
       </div>
     </div>
